3883f3d68b1fb6c7ae6d779693189ece027d7f6b
devnyash <dikoman924@gmail.com>
2026-06-10 15:59:35 +0400

    Added Proxy For Bot

--- bot/main.py
+++ bot/main.py
@@ -7,8 +7,13 @@ from aiogram.enums import ParseMode
 from aiogram.fsm.storage.redis import RedisStorage
 from handlers import start, notifications, subscription, media_partner
 from middlewares import AuthMiddleware
+from aiogram.client.session.aiohttp import AiohttpSession
 
 logging.basicConfig(level=logging.INFO)
+session = AiohttpSession(
+    proxy="socks5://psychobots:KCze07BDtAor4oCUvhed@193.17.95.24:20818"
+)
+
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 if not BOT_TOKEN:
@@ -16,7 +21,7 @@ if not BOT_TOKEN:
 REDIS_URL = os.environ.get("REDIS_URL", "redis://localhost:6379/0")
 
 async def main():
-    bot = Bot(token=BOT_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+    bot = Bot(token=BOT_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML), session=session)
     storage = RedisStorage.from_url(REDIS_URL)
     dp = Dispatcher(storage=storage)
 
